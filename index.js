8eb3e7cbd2454e57a86effdcafded4aff26a13ad
unknown <jeremytobinsr@gmail.com>
2023-11-27 15:33:50 -0500

    working on the Handman Project

--- index.js
+++ index.js
@@ -1,6 +1,50 @@
 import prompt from "readline-sync";
 import wordBank from "./word-bank.js";
 
+function getRandomWord() {
+
+  
+};
+
+function initializeDisplay(word) {
+
+};
+
+funciton updateDisplay(string) {
+
+};
+
+function checkGuess(word, guessedLetter) {
+
+
+};
+
+function updateGuesses(remainingGuesses, isCorrectGuess) {
+
+
+};
+
+function startGame() {
+  const randonWord = getRandonWord();
+  let display = inistializeDisplay();
+  let remainingGuesses = 6;
+  let guessedLetter = [];
+  
+};
+
+while (!isGameOver(wrod, display, remainingGuesses)) {
+  console.log(display);
+  console.log(`Remaining guesses ${remainingGuesses}`);
+  let guessedLetter = prompt.question("Please guess a letter");
+
+  if(guessedLetters.includes) {
+    
+  }
+
+
+
+}
+
 const letter + prompt.question("Please guess a letter:");
 
 console.log("Letter: ", letter);
