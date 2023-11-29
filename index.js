cdc54d88fe4efd6eaa719bb790d2a43e3aac2c63
unknown <jeremytobinsr@gmail.com>
2023-11-29 00:52:33 -0500

    trying to finish the Hangman Project...

--- index.js
+++ index.js
@@ -1,50 +1,124 @@
-import prompt from "readline-sync";
-import wordBank from "./word-bank.js";
+import readline from 'readline';
+import wordBank from './word-bank.js';
 
-function getRandomWord() {
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-  
+const getRandomWord = () => {
+  const randomIndex = Math.floor(Math.random() * wordBank.length);
+  return wordBank[randomIndex].toLowerCase();
 };
 
-function initializeDisplay(word) {
-
-};
-
-funciton updateDisplay(string) {
-
-};
-
-function checkGuess(word, guessedLetter) {
-
-
-};
-
-function updateGuesses(remainingGuesses, isCorrectGuess) {
-
-
-};
-
-function startGame() {
-  const randonWord = getRandonWord();
-  let display = inistializeDisplay();
-  let remainingGuesses = 6;
-  let guessedLetter = [];
-  
-};
-
-while (!isGameOver(wrod, display, remainingGuesses)) {
-  console.log(display);
-  console.log(`Remaining guesses ${remainingGuesses}`);
-  let guessedLetter = prompt.question("Please guess a letter");
-
-  if(guessedLetters.includes) {
-    
+const displayWord = (word, guessedLetters) => {
+  let displayedWord = '';
+  for (const char of word) {
+    if (guessedLetters.includes(char)) {
+      displayedWord += char;
+    } else {
+      displayedWord += '_';
+    }
   }
+  return displayedWord;
+};
 
+const drawStickman = (incorrectGuesses) => {
+  const Stickman = [ 
+    '   O',
+    '   |',
+    '  /|\','
+      / \'
+  
 
+  console.log('\nIncorrect Guesses:');
+  for (let i = 0; i < incorrectGuesses; i++) {
+    console.log(Stickman[i]);
+  }
+};
 
-}
+const isGameWon = (word, guessedLetters) => {
+  return word.split('').every(char => guessedLetters.includes(char));
+};
 
-const letter + prompt.question("Please guess a letter:");
+const startGame = () => {
+  let wins = 0;
+  let losses = 0;
 
-console.log("Letter: ", letter);
+  const playRound = () => {
+    const targetWord = getRandomWord();
+    const guessedLetters = [];
+    let incorrectGuesses = 0;
+
+    console.log('Would You Like To Play A Game?');
+    console.log('Push Down Ctrl + c To End Your Game.'); 
+
+    const playAgain = () => {
+      rl.question('Test Yourself Again?! (yes/no)): ', answer => 
+        if (answer.toLowerCase() === 'yes') {
+          playRound();
+        } else {
+          console.log('We Hope You Had Fun Playing!');
+          console.log('Wins:', wins);
+          console.log('Losses:', losses);
+          rl.close();
+        }
+      }
+    };
+
+    const playOneRound = () => {
+      console.log('\nWord:', displayWord(targetWord, guessedLetters));
+      console.log('Guessed Letters:', guessedLetters.join(', '));
+      console.log('Attempts Left:', 4 - incorrectGuesses);
+
+      rl.question('Guess A Letter: ', answer => {
+
+      const guessedLetter = answer.toLowerCase();
+
+        if (guessedLetters.includes(guessedLetter)) {
+          console.log('Nope Try Again...');
+        } 
+          else if (targetWord.includes(guessedLetter)) {
+          guessedLetters.push(guessedLetter);
+          if (isGameWon(targetWord, guessedLetters)) {
+            console.log('YaY You Got It... Your Word Was:', targetWord);
+            wins++;
+            playAgain();
+          } 
+            else {
+            playOneRound();
+          }
+        } 
+          else {
+          incorrectGuesses++;
+          drawStickman(incorrectGuesses);
+
+          if (incorrectGuesses === 6) {
+            console.log('I Am Sorry!  The Word Was:', targetWord);
+            losses++;
+            playAgain();
+          } 
+            else {
+            console.log('Well Hey There...');
+            playOneRound();
+          }
+        }
+      });
+    };
+
+    playOneRound();
+  };
+
+  playRound();
+;
+
+startGame();
+
+//look for the bold letters, those are the things you should change
+
+//also, idk if you have one yet, but you also need a word bank.js file
+//with an array
+
+//const wordBank = ['The Book'];
+export default wordBank;
+//in the array just put whatever words you want
